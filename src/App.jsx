fb5d9f315e6736aebd041a0bd9786885693160d8
Jóhannes Freyr Þorleifsson <johannesfreyr@gmail.com>
2020-01-23 21:52:20 +0000

    react context api and hooks demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
+
+//import Counter from "./views/Counter";
+import Contacts from "./views/Contacts";
+
 
 export default function App() {
-  return (
-    <div>
-      <h1>React Parcel Starter Kit</h1>
-    </div>
-  );
+	return (
+		<Container>
+			<h1>React Hooks Context Demo</h1>
+			{/* <Counter /> */}
+			<Contacts />
+		</Container>
+	);
 }
